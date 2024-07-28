0ed83604fe284caa1b53acf653cc2178c463272e
InnaChystiakova <inna_islyamova@ukr.net>
2024-07-28 13:17:34 +0100

    First commit

---
+++ EssentialFeed/EssentialFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialFeedApp.swift
+//  EssentialFeed
+//
+//  Created by Inna Chystiakova on 28/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct EssentialFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
