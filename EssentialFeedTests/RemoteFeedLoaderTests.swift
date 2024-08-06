b0b054f7603fc90697589aa4890316bc7048b9b0
InnaChystiakova <inna_islyamova@ukr.net>
2024-08-06 13:42:13 +0100

    Make HTTPClient protocol

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -10,43 +10,49 @@ import XCTest
 final class RemoteFeedLoaderTests: XCTestCase {
     
     class RemoteFeedLoader {
+        private let client: HTTPClient
+        
+        public init(client: HTTPClient) {
+            self.client = client
+        }
+        
         func load() {
-            HTTPClient.shared.get(from: URL(string: "https://a-given-url.com")!)        // Step 2: move from property to a method invoke
+            client.get(from: URL(string: "https://a-url.com")!)        // Step 2: move from property to a method invoke
         }
     }
     
-    class HTTPClient {
-        static var shared = HTTPClient()        // Step 1: not a singleton anymore
-                                                // Step 5: remove rpivate initializer, since it is not a singleton anymore
-        func get(from url: URL) {}              // Step 2: assign a parameter instead of a singleton property
+    protocol HTTPClient {
+        // Step 1: not a singleton anymore
+        // Step 5: remove rpivate initializer, since it is not a singleton anymore
+        // Step 2: assign a parameter instead of a singleton property
+        
+        func get(from url: URL)
     }
     
     
     class HTTPClientSpy: HTTPClient {           // Step 3: move the test logic to the new subclass
         var requestedURL: URL?
         
-        override func get(from url: URL) {
+        func get(from url: URL) {
             var requestedURL = url
         }
     }
 
     func testInitDoesNotRequestDataFromURL() {
         let client = HTTPClientSpy()                // Step 4: change the shared property to the subclass instance
-        HTTPClient.shared = client
-        _ = RemoteFeedLoader()
+        _ = RemoteFeedLoader(client: client)
         
         XCTAssertNil(client.requestedURL)
     }
     
     func testLoadRequestDataFromURL() {
         let client = HTTPClientSpy()                // Step 4: change the shared property to the subclass instance
-        HTTPClient.shared = client
         /*
          1. Pass the client into the constructor (constructor injection)
          2. Pass the client to the property after constructor (property injection)
          3. Pass the client as a function parameter (method/dependency injection)
         */
-        let sut = RemoteFeedLoader()
+        let sut = RemoteFeedLoader(client: client)
         
         sut.load()
         
