148d5a80e7f84397bd90c0468cc088a8b66780f4
InnaChystiakova <inna_islyamova@ukr.net>
2024-08-06 13:34:40 +0100

    Move test logic to a separate type HTTPClientSpy

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -11,25 +11,36 @@ final class RemoteFeedLoaderTests: XCTestCase {
     
     class RemoteFeedLoader {
         func load() {
-            HTTPClient.shared.requestedURL = URL(string: "https://a-given-url.com")!
+            HTTPClient.shared.get(from: URL(string: "https://a-given-url.com")!)        // Step 2: move from property to a method invoke
         }
     }
     
     class HTTPClient {
-        static let shared = HTTPClient()
-        private init() {}
+        static var shared = HTTPClient()        // Step 1: not a singleton anymore
+                                                // Step 5: remove rpivate initializer, since it is not a singleton anymore
+        func get(from url: URL) {}              // Step 2: assign a parameter instead of a singleton property
+    }
+    
+    
+    class HTTPClientSpy: HTTPClient {           // Step 3: move the test logic to the new subclass
         var requestedURL: URL?
+        
+        override func get(from url: URL) {
+            var requestedURL = url
+        }
     }
 
     func testInitDoesNotRequestDataFromURL() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()                // Step 4: change the shared property to the subclass instance
+        HTTPClient.shared = client
         _ = RemoteFeedLoader()
         
         XCTAssertNil(client.requestedURL)
     }
     
     func testLoadRequestDataFromURL() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()                // Step 4: change the shared property to the subclass instance
+        HTTPClient.shared = client
         /*
          1. Pass the client into the constructor (constructor injection)
          2. Pass the client to the property after constructor (property injection)
