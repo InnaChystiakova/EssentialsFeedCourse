c6156ff702302aec4f62bcd1f29bc5361c2149f1
InnaChystiakova <inna_islyamova@ukr.net>
2024-08-06 13:09:52 +0100

    Request data from URL on load

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,28 +8,38 @@
 import XCTest
 
 final class RemoteFeedLoaderTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    class RemoteFeedLoader {
+        func load() {
+            HTTPClient.shared.requestedURL = URL(string: "https://a-given-url.com")!
         }
+    }
+    
+    class HTTPClient {
+        static let shared = HTTPClient()
+        private init() {}
+        var requestedURL: URL?
+    }
+
+    func testInitDoesNotRequestDataFromURL() {
+        let client = HTTPClient.shared
+        _ = RemoteFeedLoader()
+        
+        XCTAssertNil(client.requestedURL)
+    }
+    
+    func testLoadRequestDataFromURL() {
+        let client = HTTPClient.shared
+        /*
+         1. Pass the client into the constructor (constructor injection)
+         2. Pass the client to the property after constructor (property injection)
+         3. Pass the client as a function parameter (method/dependency injection)
+        */
+        let sut = RemoteFeedLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
     }
 
 }
