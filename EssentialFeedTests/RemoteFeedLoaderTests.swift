9bbad0a4e574fcc18e18e5698928d7210e5b138d
InnaChystiakova <inna_islyamova@ukr.net>
2024-08-14 14:53:01 +0100

    Refactor tests to remove duplications

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -31,32 +31,38 @@ final class RemoteFeedLoaderTests: XCTestCase {
         func get(from url: URL)
     }
     
+    // MARK: - Helpers
     
-    class HTTPClientSpy: HTTPClient {           // Step 3: move the test logic to the new subclass
+   private class HTTPClientSpy: HTTPClient {           // Step 3: move the test logic to the new subclass
         var requestedURL: URL?
         
         func get(from url: URL) {
             requestedURL = url
         }
     }
+    
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()                            // Step 4: change the shared property to the subclass instance
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
+    
+    //MARK: - Tests
 
     func testInitDoesNotRequestDataFromURL() {
-        let url = URL(string: "https://a-url.com")!
-        let client = HTTPClientSpy()                // Step 4: change the shared property to the subclass instance
-        _ = RemoteFeedLoader(url: url, client: client)
+        let (_, client) = makeSUT()
         
         XCTAssertNil(client.requestedURL)
     }
     
     func testLoadRequestDataFromURL() {
-        let client = HTTPClientSpy()                // Step 4: change the shared property to the subclass instance
         /*
          1. Pass the client into the constructor (constructor injection)
          2. Pass the client to the property after constructor (property injection)
          3. Pass the client as a function parameter (method/dependency injection)
         */
         let url = URL(string: "https://a-given-url.com")!
-        let sut = RemoteFeedLoader(url: url, client: client)
+        let (sut, client) = makeSUT(url: url)
         
         sut.load()
         
