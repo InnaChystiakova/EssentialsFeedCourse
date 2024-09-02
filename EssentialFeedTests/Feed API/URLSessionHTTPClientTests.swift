06e4af3efef86443cbe4b8bfd4c355753644f889
InnaChystiakova <inna_islyamova@ukr.net>
2024-09-02 14:17:10 +0100

    Resumes data task on creation

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -15,7 +15,7 @@ class URLSessionHTTPClient {
     }
     
     func get(from url: URL) {
-        session.dataTask(with: url) {_, _, _ in }
+        session.dataTask(with: url) {_, _, _ in }.resume()
     }
 }
 class URLSessionHTTPClientTests: XCTestCase {
@@ -46,15 +46,29 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     private class URLSessionSpy: URLSession {
         var receivedURLs = [URL]()
+        private var stubs = [URL: URLSessionDataTask]()
         
         override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, (any Error)?) -> Void) -> URLSessionDataTask {
             receivedURLs.append(url)
-            return FakeURLSessionDataTask()
+            return stubs[url] ?? FakeURLSessionDataTask()
+        }
+        
+        func stub(url: URL, task: URLSessionDataTask) {
+            stubs[url] = task
         }
     }
     
-    private class FakeURLSessionDataTask: URLSessionDataTask {}
-
+    private class FakeURLSessionDataTask: URLSessionDataTask {
+        override func resume() { }
+    }
+    private class URLSessionDataTaskSpy: URLSessionDataTask {
+        var resumeCallCount = 0
+        
+        override func resume() {
+            resumeCallCount += 1
+        }
+    }
+    
     //MARK: - Tests
     
     func testGetFromURLCreatesDataTaskWithURL() {
@@ -67,4 +81,17 @@ class URLSessionHTTPClientTests: XCTestCase {
         XCTAssertEqual(session.receivedURLs, [url])
     }
     
+    func testGetFromURLResumesDataTaskWithURL() {
+        let url = URL(string: "https://any-url.com")!
+        let session = URLSessionSpy()
+        let task = URLSessionDataTaskSpy()
+        session.stub(url: url, task: task)
+        
+        let sut = URLSessionHTTPClient(session: session)
+        
+        sut.get(from: url)
+        
+        XCTAssertEqual(task.resumeCallCount, 1)
+    }
+
 }
