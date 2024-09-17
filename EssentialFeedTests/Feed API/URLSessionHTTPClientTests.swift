922d63386a04a68bb267eb9be2cc3536e67ef76c
InnaChystiakova <inna_islyamova@ukr.net>
2024-09-17 15:21:17 +0100

    Move URL creation to a shared factory method

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -114,17 +114,20 @@ class URLSessionHTTPClientTests: XCTestCase {
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
+    
+    private func anyURL() -> URL {
+        return URL(string: "https://any-url.com")!
+    }
         
     //MARK: - Tests
         
     func testGetFromURLFailsOnRequestError() {
-        let url = URL(string: "https://any-url.com")!
         let error = NSError(domain: "domain error", code: 1)
         
         URLProtocolStub.stub(data: nil, response: nil, error: error)
                 
         let exp = expectation(description: "Wait for completion")
-        makeSUT().get(from: url) { result in
+        makeSUT().get(from: anyURL()) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, error.domain)
@@ -140,7 +143,7 @@ class URLSessionHTTPClientTests: XCTestCase {
     }
     
     func testGetFromURLPerformsGETRequestWithURL() {
-        let url = URL(string: "https://any-url.com")!
+        let url = anyURL()
         let exp = expectation(description: "Wait for requset")
 
         URLProtocolStub.observeRequest { request in
