310571e3bd245c3bfc27a474702e1d0bfeca0487
InnaChystiakova <inna_islyamova@ukr.net>
2024-10-31 12:36:16 +0000

    Add 'RemoteFeedItem' data transfer representation to decouple items mapper from 'FeedItem' data models

--- EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -7,41 +7,31 @@
 
 import Foundation
 
+// internal structure that duplicates the Entity
+
+internal struct RemoteFeedItem: Decodable {
+    internal let id: UUID
+    internal let description: String?
+    internal let location: String?
+    internal let image: URL                  // keep the name as in the original JSON key
+}
+
 internal final class FeedItemsMapper {
     
     private struct Root: Decodable {
-        let items: [Item]               // decodes array of items from original JSON
-        
-        var feed: [FeedItem] {
-            return items.map { $0.item }        // takes array of items, prepared from JSON, and maps them into array of Entity items
-        }
-    }
-
-    // private structure that duplicates the Entity
-    
-    private struct Item: Decodable {
-        let id: UUID
-        let description: String?
-        let location: String?
-        let image: URL                  // keep the name as in the original JSON key
-        
-        // convert to the high level struct (Entity), where original keys are semantically clear
-        // returns the Entity item, created with passed data
-        var item: FeedItem {
-            return FeedItem(id: id, description: description, location: location, imageURL: image)
-        }
+        let items: [RemoteFeedItem]               // decodes array of items from original JSON
     }
     
     private static var OK_200: Int { return 200 }
     
     // map function overload
     
-    internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
+    internal static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
         guard response.statusCode == OK_200,
               let root = try? JSONDecoder().decode(Root.self, from: data)
         else {
-            return .failure(RemoteFeedLoader.Error.invalidData)
+            throw RemoteFeedLoader.Error.invalidData
         }
-        return .success(root.feed)
+        return root.items
     }
 }
