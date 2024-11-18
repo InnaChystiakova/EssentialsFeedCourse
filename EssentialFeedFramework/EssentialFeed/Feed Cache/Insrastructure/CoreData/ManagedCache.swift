39d1a17175324dfa10c0c87cb0425f85a3171489
InnaChystiakova <inna_islyamova@ukr.net>
2024-11-18 19:46:47 +0000

    Extract CoreData managed model classes into separate files

---
+++ EssentialFeedFramework/EssentialFeed/Feed Cache/Insrastructure/CoreData/ManagedCache.swift
@@ -0,0 +1,8 @@
+//
+//  ManagedCache.swift
+//  EssentialFeedFramework
+//
+//  Created by Inna Chystiakova on 18/11/2024.
+//
+
+import Foundation
---
+++ EssentialFeedFramework/EssentialFeed/Feed Cache/Insrastructure/CoreData/ManagedFeedImage.swift
@@ -0,0 +1,8 @@
+//
+//  ManagedFeedImage.swift
+//  EssentialFeedFramework
+//
+//  Created by Inna Chystiakova on 18/11/2024.
+//
+
+import Foundation
