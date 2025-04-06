188661b24b22497d53cdec5514d01a42207f5481
InnaChystiakova <inna_islyamova@ukr.net>
2025-04-06 16:39:27 +0100

    Move 'FeedViewController' to production target

---
+++ EssentialFeediOS/FeedViewController.swift
@@ -0,0 +1,45 @@
+//
+//  FeedViewController.swift
+//  
+//
+//  Created by Inna Chystiakova on 06/04/2025.
+//
+
+import UIKit
+import EssentialFeedFramework
+
+final public class FeedViewController: UITableViewController {
+    private var loader: FeedLoader?
+    private var onViewIsAppearing: ((FeedViewController) -> Void)?
+    
+    public convenience init(loader: FeedLoader) {
+        self.init()
+        self.loader = loader
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+        
+        onViewIsAppearing = { [weak self] vc in
+            self?.refreshControl?.beginRefreshing()
+        }
+        
+        load()
+    }
+    
+    public override func viewIsAppearing(_ animated: Bool) {
+        super.viewIsAppearing(animated)
+        
+        onViewIsAppearing?(self)
+    }
+    
+    @objc private func load() {
+        onViewIsAppearing?(self)
+        loader?.load { [weak self] _ in
+            self?.refreshControl?.endRefreshing()
+        }
+    }
+}
--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -8,42 +8,7 @@
 import XCTest
 import UIKit
 import EssentialFeedFramework
-
-final class FeedViewController: UITableViewController {
-    private var loader: FeedLoader?
-    private var onViewIsAppearing: ((FeedViewController) -> Void)?
-    
-    convenience init(loader: FeedLoader) {
-        self.init()
-        self.loader = loader
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        refreshControl = UIRefreshControl()
-        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
-        
-        onViewIsAppearing = { [weak self] vc in
-            self?.refreshControl?.beginRefreshing()
-        }
-        
-        load()
-    }
-    
-    override func viewIsAppearing(_ animated: Bool) {
-        super.viewIsAppearing(animated)
-        
-        onViewIsAppearing?(self)
-    }
-    
-    @objc private func load() {
-        onViewIsAppearing?(self)
-        loader?.load { [weak self] _ in
-            self?.refreshControl?.endRefreshing()
-        }
-    }
-}
+import EssentialFeediOS
 
 final class FeedViewControllerTests: XCTestCase {
 
