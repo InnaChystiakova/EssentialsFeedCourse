22d5a5ac899826742a35e09d1f360d88a7cdc761
InnaChystiakova <inna_islyamova@ukr.net>
2025-04-09 21:31:51 +0100

    Remove 'EssentialFeedFramework' module import from 'FeedViewController' file since it does not depend on any 'EssentialFeedFramework' component.

--- EssentialFeediOS/FeedUI/Controllers/FeedViewController.swift
+++ EssentialFeediOS/FeedUI/Controllers/FeedViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import EssentialFeedFramework
 
 final public class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching {
     private var refreshController: FeedRefreshViewController?
