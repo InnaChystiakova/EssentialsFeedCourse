53afe8be5555415a4fb053b9f4758535c2fe0ff5
InnaChystiakova <inna_islyamova@ukr.net>
2025-04-20 17:53:44 +0100

    Move ErrorView to separate file

---
+++ EssentialFeediOS/FeedUI/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeed
+//
+//  Created by Inna Chystiakova on 20/04/2025.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
