6a9bd563051f8ba74a7b20e2728036b38115ac77
InnaChystiakova <inna_islyamova@ukr.net>
2025-04-20 19:40:47 +0100

    Move FeedErrorViewModel to separate file

---
+++ EssentialFeedFramework/EssentialFeed/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeed
+//
+//  Created by Inna Chystiakova on 20/04/2025.
+//
+
+public struct FeedErrorViewModel {
+    public let message: String?
+    
+    static var noError: FeedErrorViewModel {
+        return FeedErrorViewModel(message: nil)
+    }
+    static func error(message: String) -> FeedErrorViewModel {
+        return FeedErrorViewModel(message: message)
+    }
+}
