493967d76b92ae701eab44e7cb46a2512902394a
InnaChystiakova <inna_islyamova@ukr.net>
2025-04-21 21:11:06 +0100

    RemoteFeedImageDataLoader.loadImageDataFromURL does not deliver result after instance has been deallocated

--- EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -20,7 +20,9 @@ class RemoteFeedImageDataLoader {
     }
     
     func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) {
-        client.get(from: url) { result in
+        client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+            
             switch result {
             case let .success(data, response):
                 if response.statusCode == 200, !data.isEmpty {
@@ -98,6 +100,20 @@ final class RemoteFeedImageDataLoaderTests: XCTestCase {
         expect(sut, toCompleteWith: .success(nonEmptyData), when: {
             client.complete(withStatusCodee: 200, data: nonEmptyData)
         })
+    }
+    
+    func testLoadImageDataFromURLDoesNotDeliversResultAfterSUTInstanceHasBeenDeallocated() {
+        let client = HTTPClientSpy()
+        var sut: RemoteFeedImageDataLoader? = RemoteFeedImageDataLoader(client: client)
+        
+        var capturedResult = [FeedImageDataLoader.Result]()
+        
+        sut?.loadImageData(from: anyURL()) { capturedResult.append($0) }
+        
+        sut = nil
+        client.complete(withStatusCodee: 200, data: anyData())
+        
+        XCTAssertTrue(capturedResult.isEmpty)
     }
     
     // MARK: - Helpers
