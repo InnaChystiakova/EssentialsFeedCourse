cb976903d571ccbda8ef8569a5108d36571a799a
InnaChystiakova <inna_islyamova@ukr.net>
2025-04-23 11:12:07 +0100

    Add RemoteFeedImageDataLoader end-to-end test to guarantee both the client (app) and server (backend) respects the API contract, so we can fetch image data from remote.

--- EssentialFeedFramework/EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
+++ EssentialFeedFramework/EssentialFeed/Feed API/RemoteFeedImageDataLoader.swift
@@ -10,7 +10,7 @@ import Foundation
 public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
     private let client: HTTPClient
     
-    init(client: HTTPClient) {
+    public init(client: HTTPClient) {
         self.client = client
     }
     
