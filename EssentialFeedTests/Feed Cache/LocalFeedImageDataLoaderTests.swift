1c2adbca5986abf649b93e5a25f1e9f56cedae40
InnaChystiakova <inna_islyamova@ukr.net>
2025-04-26 13:09:37 +0100

    LocalFeedImageDataLoader.loadImageData requests stored data for URL from FeedImageDataStore

--- EssentialFeedTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
+++ EssentialFeedTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
@@ -8,9 +8,23 @@
 import XCTest
 @testable import EssentialFeedFramework
 
-final class LoadFeedImageDataLoader {
-    init(store: Any) {
-        
+protocol FeedImageDataStore {
+    func retrieve(dataForURL url: URL)
+}
+
+final class LoadFeedImageDataLoader: FeedImageDataLoader {
+    private struct Task: FeedImageDataLoaderTask {
+        func cancel() {}
+    }
+    
+    private let store: FeedImageDataStore
+    init(store: FeedImageDataStore) {
+        self.store = store
+    }
+    
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        store.retrieve(dataForURL: url)
+        return Task()
     }
 }
 
@@ -20,6 +34,15 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
         
         XCTAssertTrue(store.receivedMessages.isEmpty)
 
+    }
+    
+    func testLoadImagedataFromURLRequestsStoreDataForURL() {
+        let (sut, store) = makeSUT()
+        let url = anyURL()
+        
+        _ = sut.loadImageData(from: url) { _ in }
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve(dataFor: url)])
     }
     
     // MARK: - Helpers
@@ -32,7 +55,15 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
         return (sut, store)
     }
     
-    private class FeedStoreSpy {
-        let receivedMessages = [Any]()
+    private class FeedStoreSpy: FeedImageDataStore {
+        enum Message: Equatable {
+            case retrieve(dataFor: URL)
+        }
+        
+        private(set) var receivedMessages = [Message]()
+        
+        func retrieve(dataForURL url: URL) {
+            receivedMessages.append(.retrieve(dataFor: url))
+        }
     }
 }
