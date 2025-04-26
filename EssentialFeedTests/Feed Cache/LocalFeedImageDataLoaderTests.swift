258b60134ee9bdc4fa3acd99428622f593ff9d00
InnaChystiakova <inna_islyamova@ukr.net>
2025-04-26 13:37:22 +0100

    LocalFeedImageDataLoader.loadImageData delivers stored data when store finds image data for url

--- EssentialFeedTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
+++ EssentialFeedTests/Feed Cache/LocalFeedImageDataLoaderTests.swift
@@ -33,7 +33,7 @@ final class LocalFeedImageDataLoader: FeedImageDataLoader {
         store.retrieve(dataForURL: url) { result in
             completion(result
                 .mapError {_ in Error.failed}
-                .flatMap {_ in .failure(Error.notFound)}
+                .flatMap { data in data.map{ .success($0) } ?? .failure(Error.notFound) }
             )
         }
         return Task()
@@ -66,12 +66,21 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
         })
     }
     
-    func testloadImageDataFromURLDeliversNotFoundErrorOnNotFound() {
+    func testLoadImageDataFromURLDeliversNotFoundErrorOnNotFound() {
         let (sut, store) = makeSUT()
         
         expect(sut, toCompleteWith: notFound(), when: {
             store.complete(with: .none)
         })
+    }
+    
+    func testLoadImagDataFromURLDeliversStoreedDataOnFoundData() {
+        let (sut, store) = makeSUT()
+        let foundData = anyData()
+        
+        expect(sut, toCompleteWith: .success(foundData)) {
+            store.complete(with: foundData)
+        }
     }
     
     // MARK: - Helpers
