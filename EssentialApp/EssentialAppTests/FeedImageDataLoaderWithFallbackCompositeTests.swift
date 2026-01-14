c4559cfceb2d59bab1bd42af19bca7e828163c67
InnaChystiakova <inna_islyamova@ukr.net>
2026-01-14 20:50:17 +0000

    Add FeedImageDataLoaderWithFallbackCompositeTests file

---
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -0,0 +1,13 @@
+//
+//  FeedImageDataLoaderWithFallbackCompositeTests.swift
+//  EssentialApp
+//
+//  Created by Inna Chystiakova on 14/01/2026.
+//
+
+import XCTest
+@testable import EssentialFeedFramework
+
+final class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
+    
+}
